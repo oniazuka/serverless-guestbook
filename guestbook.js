33d41af5ac71ec1247025406d021ff01697ac1ef
oniazuka <110148298+oniazuka@users.noreply.github.com>
2022-07-28 12:06:03 +0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://88856d51.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
